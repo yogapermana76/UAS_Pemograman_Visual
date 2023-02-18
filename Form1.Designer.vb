492e385872e819b3f4316fe4cb89d024570c092c
Yoga Permana <42025274+yogapermana76@users.noreply.github.com>
2023-02-18 12:35:28 +0700

    init

---
+++ Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtContent = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.btnCount = New System.Windows.Forms.Button()
+        Me.txtCount = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtContent
+        '
+        Me.txtContent.Location = New System.Drawing.Point(38, 76)
+        Me.txtContent.Multiline = True
+        Me.txtContent.Name = "txtContent"
+        Me.txtContent.Size = New System.Drawing.Size(214, 117)
+        Me.txtContent.TabIndex = 1
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(268, 38)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 5
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.Location = New System.Drawing.Point(38, 38)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.Size = New System.Drawing.Size(214, 23)
+        Me.txtFilePath.TabIndex = 6
+        '
+        'btnCount
+        '
+        Me.btnCount.Location = New System.Drawing.Point(38, 208)
+        Me.btnCount.Name = "btnCount"
+        Me.btnCount.Size = New System.Drawing.Size(112, 23)
+        Me.btnCount.TabIndex = 7
+        Me.btnCount.Text = "Count Words"
+        Me.btnCount.UseVisualStyleBackColor = True
+        '
+        'txtCount
+        '
+        Me.txtCount.Location = New System.Drawing.Point(156, 208)
+        Me.txtCount.Name = "txtCount"
+        Me.txtCount.Size = New System.Drawing.Size(96, 23)
+        Me.txtCount.TabIndex = 8
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(38, 237)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(214, 23)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtCount)
+        Me.Controls.Add(Me.btnCount)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtContent)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtContent As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtFilePath As TextBox
+    Friend WithEvents btnCount As Button
+    Friend WithEvents txtCount As TextBox
+    Friend WithEvents btnClear As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub txtFilePath_TextChanged(sender As Object, e As EventArgs) Handles txtFilePath.TextChanged
+
+    End Sub
+
+    Private Sub textContent_TextChanged(sender As Object, e As EventArgs) Handles txtContent.TextChanged
+
+    End Sub
+
+    Private Sub txtCount_TextChanged(sender As Object, e As EventArgs) Handles txtCount.TextChanged
+
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        'Menampilkan dialog untuk memillih file
+        Dim openFileDialog As New OpenFileDialog()
+
+        'Mengatur filter agar hanya menampilkan file text
+        openFileDialog.Filter = "Text (*.txt)|*.txt"
+
+        'Menampilkan dialog dan memeriksa apakah pengguna menekan tombol OK
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            'Menampilkan path file yang dipilih di TextBox
+            txtFilePath.Text = openFileDialog.FileName
+
+            'Membaca isi file dan menampilkan di TextBox
+            Dim fileReader As StreamReader = New StreamReader(openFileDialog.FileName)
+            txtContent.Text = fileReader.ReadToEnd()
+            fileReader.Close()
+        End If
+    End Sub
+
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+        'Menghitung jumlah kata dalam teks yang dimasukkan
+        Dim text As String = txtContent.Text
+        Dim wordCount As Integer = 0
+
+        'Membuat array kata dari teks yang dimasukkan
+        Dim words() As String = text.Split(" "c)
+
+        'Menghitung jumlah kata dalam array
+        For Each word As String In words
+            wordCount += 1
+        Next
+
+        'Menampilkan pesan jika teks tidak mengandung kata
+        If wordCount = 1 Then
+            wordCount = 0
+            MessageBox.Show("Teks tidak mengandung kata")
+        End If
+
+        'Menampilkan hasil perhitungan di TextBox
+        txtCount.Text = wordCount.ToString()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Menghapus isi TextBox
+        txtFilePath.Clear()
+        txtContent.Clear()
+        txtCount.Clear()
+    End Sub
+End Class
